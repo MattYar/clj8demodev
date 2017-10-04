ab0f95174ff2415b18d3ba4962b15ab8e117759f
Colten Krauter <ckrauter@starbucks.com>
2017-10-04 06:31:40 -0700

    Updating style

--- ping.php
+++ ping.php
@@ -9,9 +9,9 @@ $data = curl_exec($ch);
 $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 if($httpcode>=200 && $httpcode<300){
-  echo 'worked';
+  echo 'Online';
 } else {
-  echo "didn't work";
+  echo 'Unresponsive';
 }
 
 ?>
