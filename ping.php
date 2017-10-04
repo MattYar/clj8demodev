49aeb534289010100e34b7cdf687922ed5f0be54
Colten Krauter <ckrauter@starbucks.com>
2017-10-04 05:49:06 -0700

    Updating ping

--- ping.php
+++ ping.php
@@ -1,12 +1,11 @@
 <?php
 function pingAddress($ip) {
-    $pingresult = exec("/bin/ping -n 3 $ip", $outcome, $status);
-    if (0 == $status) {
-        $status = "alive";
+    $ping = exec("ping -n 2 $ip", $output, $status);
+    if (strpos($output[2], 'unreachable') !== FALSE) {
+        return '<span style="color:#f00;">OFFLINE</span>';
     } else {
-        $status = "dead";
+        return '<span style="color:green;">ONLINE</span>';
     }
-    echo "The IP address, $ip, is  ".$status;
 }
 
 pingAddress("127.0.0.1");
